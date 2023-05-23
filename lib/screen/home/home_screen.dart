cb49452a670d2dbb9bff9fae056b8ba8b1032e74
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-05-23 13:55:29 +0900

    add: 基本構成の追加 #2

---
+++ lib/screen/home/home_screen.dart
@@ -0,0 +1,11 @@
+// 最初に表示される画面
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
