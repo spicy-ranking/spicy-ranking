c448d614240add516d5b78615436bf5cacd3709b
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-06-04 19:40:47 +0900

    update: firebaseのデータをプルダウンに表示 #8

--- lib/components/drop_box_product_menu.dart
+++ lib/components/drop_box_product_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spicy_ranking/constant/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 const  List<String> choices = <String>['ピーヤング 激辛春雨END', 'ペヤング 激辛焼きそばEND', 
 '三養食品 ブルダック炒め麺', '蒙古タンメン中本 北極ラーメン 激辛味噌', '蒙古タンメン中本 辛旨味噌'];
@@ -12,26 +13,47 @@ class DropdownButtonProductMenu extends StatefulWidget {
 }
 
 class _DropdownButtonMenuState extends State<DropdownButtonProductMenu> {
-  String isSelectedValue = choices.first;
+  String isSelectedValue = "0";
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      iconSize: 30,
-      padding: const EdgeInsets.all(kDefaultPaddin),
-      items: choices.map<DropdownMenuItem<String>>((String list) {
-        return DropdownMenuItem<String>(
-          value: list,
-          child: Text(list),
-        );
-      }).toList(),
-      value: isSelectedValue,
-      onChanged: (String? value) {
-        setState(() {
-          isSelectedValue = value!;
-        });
-      },
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('spicy-cup-noodle').snapshots(),
+      builder: (context, snapshot){
+        List<DropdownMenuItem> foodItems = [];
+        if(!snapshot.hasData){
+          const CircularProgressIndicator();
+        } else {
+          final foods = snapshot.data?.docs.reversed.toList();
+          foodItems.add(const DropdownMenuItem(
+            value: "0",
+            child: Text('Select Food'),
+            ),
+          );
+
+          for (var food in foods!){
+            foodItems.add(
+              DropdownMenuItem(
+                value: food.id,
+                child: Text(food['name'],),
+              ),
+            );
+          }
+        }
+        return DropdownButton(
+        iconSize: 30,
+        padding: const EdgeInsets.all(kDefaultPaddin),
+        items: foodItems,
+        value: isSelectedValue,
+        onChanged: (foodValue) {
+          setState(() {
+            isSelectedValue = foodValue!;
+          });
+        },
+      );
+      }
     );
+    
   }
 }
 
