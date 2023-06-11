99c15dd4fe797a26186a4cf3de8f18b71b5b00c4
akkicoke <takiguchi.akihisa@arakawa-lab.com>
2023-06-11 17:03:48 +0900

    いいねボタンカウント＋データ反映

--- lib/view/history_page.dart
+++ lib/view/history_page.dart
@@ -1,16 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+
 class History { //履歴クラス
+  late String id; // ドキュメントID
   late String hot; //辛いもの
   late String cold; //辛くないもの
   late int good; //賛成数
   late int bad; //反対数
 
-  History({required this.hot, required this.cold, required this.good, required this.bad});
+  History({required this.id, required this.hot, required this.cold, required this.good, required this.bad});
 
-  factory History.fromMap(Map<String, dynamic> data) {
-    return History(hot: data['hot'], cold: data['cold'], good: data['good'], bad: data['bad']);
+  factory History.fromMap(String id, Map<String, dynamic> data) {
+    return History(id: id, hot: data['hot'], cold: data['cold'], good: data['good'], bad: data['bad']);
   }
 }
 
@@ -21,8 +23,10 @@ class HistoryPage extends StatelessWidget {
   Stream<List<History>> _fetchHistorysStream() {
     final firestore = FirebaseFirestore.instance;
     final stream = firestore.collection('history').snapshots();
-    return stream.map((snapshot) =>
-        snapshot.docs.map((doc) => History.fromMap(doc.data())).toList());
+    return stream.map((snapshot) => snapshot.docs.map((doc) {
+          final historyId = doc.id;
+          return History.fromMap(historyId, doc.data());
+        }).toList());
   }
 
 
@@ -55,18 +59,31 @@ class HistoryPage extends StatelessWidget {
               subtitle: Text('🙂: ${history.cold}'),
               leading: const Icon(Icons.account_circle),
               trailing: Wrap(
-                
                 children: [
                   OutlinedButton.icon(
-                    onPressed:() {},
+                    onPressed:() async{
+                      CollectionReference historyCollection = FirebaseFirestore.instance.collection('history');
+                      // クリックされた履歴のIDを取得
+                      await historyCollection.doc(history.id).update({
+                        'good': history.good + 1,
+                      });
+                    },
                     icon: const Icon(Icons.thumb_up),
                     label:Text('${history.good}'),
                     style:OutlinedButton.styleFrom(
                       primary:Colors.red,
                     )
+                    
                   ),
+                  
                   OutlinedButton.icon(
-                    onPressed:() {},
+                    onPressed:() async{
+                      CollectionReference historyCollection = FirebaseFirestore.instance.collection('history');
+                      // クリックされた履歴のIDを取得
+                      await historyCollection.doc(history.id).update({
+                        'bad': history.bad + 1,
+                    });
+                    },
                     icon: const Icon(Icons.thumb_down),
                     label:Text('${history.bad}'),
                     style:OutlinedButton.styleFrom(
