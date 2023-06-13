b8b1793f5b847777fc58104aee5a0647be278bf9
mori toshiki <toshiki@morinoMacBook-Air.local>
2023-06-13 18:33:36 +0900

    add: グリコ2レーティングの実装 #14

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 from firebase_admin import firestore
 import firebase_admin
 from firebase_admin import credentials
+import glicko2_rating
+import copy
 
 def main():
     JSON_PATH = './python/private_key.json'
@@ -13,10 +15,64 @@ def main():
     doc_ref = db.collection('history')
     docs = doc_ref.stream()
 
-    for doc in docs:
-        print("----対戦----")
-        print("辛くない：", doc.get('cold'))
-        print("辛い：", doc.get('hot'))
+    # for doc in docs:
+    #     print("----対戦----")
+    #     print("辛くない：", doc.get('cold'))
+    #     print("辛い：", doc.get('hot'))
+    
+    # player設定(ここはfirebaseから持ってくる)
+    player1 = setPlayer(1400,30,0.06)
+    player2 = setPlayer(1550,100,0.06)
+    
+    # 計算できるように成型
+    players = [player1, player2]
+    # 1: win, 2: lose
+    ranks=[2,1]
+
+    newPlayers = calcRatings(players,ranks)
+
+    print("===loser=== \n rating: ",newPlayers[0].rating)
+    print("===winner=== \nrating: ",newPlayers[1].rating)
+
+    
+
+# プレイヤーのパラメータを設定する関数
+## 変数rating, rd, volはそれぞれrating、rating deviation、rating volatilityの初期値
+## playerオブジェクトを1つ返す
+def setPlayer(rating=1500, rd=350, vol=0.06):
+    player = glicko2_rating.Player()
+    player.rating=rating
+    player.rd=rd
+    player.vol=vol
+    return player
+
+# 複数プレイヤーのrating等パラメータを計算する関数
+## 変数playersは複数のplayerオブジェクトを格納したリスト
+## 変数ranksは複数のplayerの対戦時における順位を格納したリスト
+## 各playerのrating等を更新してplayersリストを返す
+def calcRatings(players,ranks):
+    newPlayers=[]
+    for i, (target_player,target_rank) in enumerate(zip(players,ranks)):
+        new_target_player = copy.deepcopy(target_player)
+        ratings=[]
+        rds=[]
+        outcomes=[]
+        for j, (player, rank) in enumerate(zip(players,ranks)):
+            if not i==j:
+                ratings.append(player.rating)
+                rds.append(player.rd)
+                if rank>target_rank:
+                    outcomes.append(1)
+                elif rank<target_rank:
+                    outcomes.append(0)
+                elif rank==target_rank:
+                    outcomes.append(0.5)
+        
+        new_target_player.update_player(ratings, rds, outcomes)
+        newPlayers.append(new_target_player)
+    
+    return newPlayers
+
 
 if __name__ == '__main__':
     main()
